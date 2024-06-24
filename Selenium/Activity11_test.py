4329df49060f27f95e46a9a74bb7980f4a43da02
Bommire <154328147+Bommire@users.noreply.github.com>
2024-06-24 19:25:31 +0530

    Add files via upload

--- Selenium/Activity11_test.py
+++ Selenium/Activity11_test.py
@@ -1,17 +1,13 @@
-
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.firefox.service import Service as FirefoxService 
 from webdriver_manager.firefox import GeckoDriverManager
 service = FirefoxService(GeckoDriverManager().install())
 with webdriver.Firefox(service=service) as driver:
-    driver.get("https://v1.training-support.net/selenium/input-events")
+    driver.get("https://v1.training-support.net/selenium/dynamic-controls")
     print("Page title is: ", driver.title)
-    actions = ActionChains(driver)
-    actions.send_keys("A") 
-    actions.key_down(Keys.CONTROL) 
-    actions.send_keys("a") 
-    actions.send_keys("c") 
-    actions.key_up(Keys.CONTROL) 
-    actions.perform() 
-
+    checkbox = driver.find_element(By.CSS_SELECTOR, "input.willDisappear")
+    checkbox.click()
+    print("Checkbox is selected: ", checkbox.is_selected())
+    checkbox.click()
+    print("Checkbox is selected: ", checkbox.is_selected())
